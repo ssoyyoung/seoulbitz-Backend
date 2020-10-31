c74430e8bedaf97619d43fdfb85a09f83d2d8bdb
ssoyyoung <ssoyyoung.p@gmail.com>
2020-11-01 00:34:06 +0900

    adding tls to main.go

--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	router "github.com/ssoyyoung.p/seoulbitz-Backend/router"
+	"fmt"
 )
 
 func main() {
+	debug := true
 	echoR := router.Router()
 
-	echoR.Logger.Fatal(echoR.Start(":1323"))
+	fmt.Println("Start echo server..")
+
+	if debug {
+		echoR.Logger.Fatal(echoR.Start(":1323"))
+	} else {
+		echoR.Logger.Fatal(echoR.StartTLS(":1323", "cert.pem", "privkey.pem"))
+	}
 }
